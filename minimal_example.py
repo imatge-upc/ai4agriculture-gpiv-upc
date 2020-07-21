28e28d18ba87e8f41f20bad66b95aa8bddd1343d
Josep Ramon Morros <ramon.morros@upc.edu>
2020-07-21 20:01:00 +0200

    first version

---
+++ minimal_example.py
@@ -0,0 +1,62 @@
+import cv2
+import sys
+import os
+
+''' 
+This script reads an image with two Aruco markers: one to identify the tree and the second one to identify the orientation.
+The image file is renamed using the tree number and orientation
+'''
+
+name = 'images/test_image.jpg'
+
+parameters =  aruco.DetectorParameters_create()
+
+# Load custom dictionary
+dict_file = 'CUSTOM_ARUCO_MARKERS_DICT_8x8_1024.pkl'
+if os.path.isfile(dict_file):
+    print ('Loading existing dictionary')
+    with open(dict_file, 'rb') as fd:
+          cdic_serializable = pickle.load(fd)
+
+    cdic = aruco.Dictionary_create(5,3)
+    cdic.markerSize = cdic_serializable['markerSize']
+    cdic.maxCorrectionBits = cdic_serializable['maxCorrectionBits']
+    cdic.bytesList = cdic_serializable['bytesList']
+else:
+    print ('ERROR: Could not read dictionary file!')
+    sys.exit()
+
+# Read test image
+ima = cv2.imread(name)
+gray = cv2.cvtColor(ima, cv2.COLOR_BGR2GRAY)
+h,w = ima.shape[0:2]
+
+corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, cdic, parameters=parameters)
+print ('Markers found in image: ', ids)
+
+if ids is None or len(ids) != 2:
+    print ('ERROR: Incorrect number of markers detected. Two markers should appear in the image')
+    sys.exit()
+
+ids.sort()
+orientation = None
+code = -1
+
+if ids[0] == 0:
+    orientation = 'Front'
+    code = ids[1]
+if ids[1] == 511:
+    if orientation is not None:
+        print ('ERROR: Front and Back markers detected! Only one should appear in the image')
+        sys.exit()
+    else:
+        orientation = 'Back'
+        code = ids[0]
+
+if code == 0 or code == 511:
+    print ('ERROR: Wrong code. Valid codes are in the range [1-510]')
+    sys.exit()
+
+# Rename image 
+output_name = 'Tree_{:05d}_{}.jpg'.format(code,orientation)
+cv2.imwrite(output_name, ima)
